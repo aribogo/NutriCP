e537186a6ce909c55db0dc0e41f863e3166f025e
aribogo <ariadne.bogo@gmail.com>
2022-11-17 20:12:31 -0300

    error: download pdf

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,20 +1,43 @@
-import { StyleSheet, Text, ScrollView, View, Link } from "react-native";
+import { StyleSheet, Text, ScrollView, View, Link, TouchableOpacity } from "react-native";
 import { scale } from "../../utils/screenResizing";
+import { Constants, FileSystem } from 'expo';
+import RNFS from 'react-native-fs';
+
 
 const Container = (props) => {
 
   function ShowExamplesByType(examples) {
-    examples.map(() => (example) => 
-      <Examples example={example}/>
+    return (
+      <View>
+        {examples.map((item, key) => (
+          item.isInternal === true ? <Text  key={key}>botão</Text> : 
+          
+          <TouchableOpacity onPress={() => {
+            download();
+           }}
+           style={{
+             height:(50),
+             shadowOffset: {width: 0, height: 0},
+             shadowOpacity: 1,
+             shadowRadius:(8),
+             elevation:(8),
+             borderRadius:(25),
+             alignItems: 'center',
+             justifyContent: 'space-between',
+             flexDirection: 'row',
+             paddingLeft:(20),
+             paddingRight:(20),
+           }}>
+           <Text
+            >
+           Download {item.title}
+      </Text>
+</TouchableOpacity>
+          
+        ))}
+      </View>
     );
-  };
-
-  const Examples = ({ example }) => {
-    return (<View>
-      {this.isInternal == true? <Text>botão</Text>: <Text>link</Text> }
-      {example.title}
-      </View>);
-  };
+  }
 
   return (
     <ScrollView
@@ -44,5 +67,22 @@ const stylesContainer = StyleSheet.create({
     marginRight: scale(10),
   },
 });
+
+async function download() {
+  let documentDir = RNFS.DocumentDirectoryPath;
+  console.log("AQUI " + documentDir);
+
+  FileSystem.downloadAsync(
+    'http://www.africau.edu/images/default/sample.pdf',
+    documentDir + 'sample.pdf'
+  )
+    .then(({ uri }) => {
+      console.log('Finished downloading to ', uri);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
 
 export default Container;

c0071fb4905706c3937f8ba3e2971657b0728236
aribogo <ariadne.bogo@gmail.com>
2022-11-20 19:05:12 -0300

    feat: add data

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,40 +1,47 @@
-import { StyleSheet, Text, ScrollView, View, Link, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  View,
+  Link,
+  TouchableOpacity,
+} from "react-native";
 import { scale } from "../../utils/screenResizing";
-import { Constants, FileSystem } from 'expo';
-import RNFS from 'react-native-fs';
-
+import Constants from "expo-constants";
+import * as FileSystem from "expo-file-system";
+import { StorageAccessFramework } from "expo-file-system";
+import base64 from "base64-js";
 
 const Container = (props) => {
-
   function ShowExamplesByType(examples) {
     return (
       <View>
-        {examples.map((item, key) => (
-          item.isInternal === true ? <Text  key={key}>botão</Text> : 
-          
-          <TouchableOpacity onPress={() => {
-            download();
-           }}
-           style={{
-             height:(50),
-             shadowOffset: {width: 0, height: 0},
-             shadowOpacity: 1,
-             shadowRadius:(8),
-             elevation:(8),
-             borderRadius:(25),
-             alignItems: 'center',
-             justifyContent: 'space-between',
-             flexDirection: 'row',
-             paddingLeft:(20),
-             paddingRight:(20),
-           }}>
-           <Text
+        {examples.map((item, key) =>
+          item.isInternal === true ? (
+            <Text key={key}>botão</Text>
+          ) : (
+            <TouchableOpacity
+              onPress={() => {
+                download();
+              }}
+              style={{
+                height: 50,
+                shadowOffset: { width: 0, height: 0 },
+                shadowOpacity: 1,
+                shadowRadius: 8,
+                elevation: 8,
+                borderRadius: 25,
+                alignItems: "center",
+                justifyContent: "space-between",
+                flexDirection: "row",
+                paddingLeft: 20,
+                paddingRight: 20,
+              }}
             >
-           Download {item.title}
-      </Text>
-</TouchableOpacity>
-          
-        ))}
+              <Text>Download {item.title}</Text>
+            </TouchableOpacity>
+          )
+        )}
       </View>
     );
   }
@@ -69,20 +76,61 @@ const stylesContainer = StyleSheet.create({
 });
 
 async function download() {
-  let documentDir = RNFS.DocumentDirectoryPath;
-  console.log("AQUI " + documentDir);
+  let fileName = "sample.pdf";
+
+  const permissions =
+    await StorageAccessFramework.requestDirectoryPermissionsAsync();
+  if (!permissions.granted) {
+    return;
+  }
 
   FileSystem.downloadAsync(
-    'http://www.africau.edu/images/default/sample.pdf',
-    documentDir + 'sample.pdf'
+    "http://www.africau.edu/images/default/sample.pdf",
+    FileSystem.documentDirectory + fileName
   )
     .then(({ uri }) => {
-      console.log('Finished downloading to ', uri);
+      console.log("Finished downloading to ", uri);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
     });
+
+  const base64Data = await fileToBase64(FileSystem.documentDirectory + fileName);
+
+  try {
+    await StorageAccessFramework.createFileAsync(
+      permissions.directoryUri,
+      fileName,
+      "application/pdf"
+    )
+      .then(async (uri) => {
+        await FileSystem.writeAsStringAsync(uri, base64Data, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } catch (e) {
+    throw new Error(e);
+  }
 }
 
+async function fileToBase64(uri) {
+  try {
+    const content = await FileSystem.readAsStringAsync(uri);
+    return base64.fromByteArray(stringToUint8Array(content));
+  } catch (e) {
+    console.warn("erro: ", e.message);
+    return "";
+  }
+}
+
+function stringToUint8Array(str) {
+  const length = str.length;
+  const array = new Uint8Array(new ArrayBuffer(length));
+  for (let i = 0; i < length; i++) array[i] = str.charCodeAt(i);
+  return array;
+}
 
 export default Container;
